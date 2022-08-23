cec13e0a941576d94ec9aaf16534dae25178b803
Pavan <pavanchambhare88@gmail.com>
2022-08-23 20:26:19 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+  <>
+  <div className="app-container">
+     <h3>First Component</h3>
+     <p>This Is a  tag </p>
+     </div>
+  </>
+  );
+}
+
+export default App;
